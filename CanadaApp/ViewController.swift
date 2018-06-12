9bdb172edc3b79a8d0a2d61a336f444f67cc8ad3
Bjoern Kaczmarek <bjoern.kaczmarek@gmail.com>
2018-06-12 18:20:00 +1000

    Initial Commit

---
+++ CanadaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CanadaApp
+//
+//  Created by Björn Kaczmarek on 12.06.18.
+//  Copyright © 2018 Björn Kaczmarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
